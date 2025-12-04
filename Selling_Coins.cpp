20deeeb2986f804f59c9877a018b8118bce3e57e
{{your_name}} <tauhidrana00@gmail.com>
2025-12-04 06:49:42 +0600

    CPP  problem solved

---
+++ Selling_Coins.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int gold = 2 * b;
+    int ans = gold + a;
+    cout << ans << endl;
+    return 0;
+}
