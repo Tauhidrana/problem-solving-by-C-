63c9ca581d611d08a0a18e8ead70766b4b166823
{{your_name}} <tauhidrana00@gmail.com>
2025-12-06 11:32:11 +0600

    CPP  problem solved

---
+++ Equalizing_Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+ int t;
+ cin>>t;
+ for (int i = 0; i < t; i++)
+ {
+    int a,b;
+    cin>>a>>b;
+    if (a==b)
+    {
+       cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+ }
+  
+    return 0;
+}
