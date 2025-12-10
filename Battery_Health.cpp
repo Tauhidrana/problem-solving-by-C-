43c37fd5e72acd30792ec5745e9dced5ea06884e
{{your_name}} <tauhidrana00@gmail.com>
2025-12-10 22:05:49 +0600

    CPP  problem solved

---
+++ Battery_Health.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int b_h;
+        cin >> b_h;
+        if (b_h >= 80)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Biryani_classes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, x;
+        cin >> n >> x;
+        int ans = n * x;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
